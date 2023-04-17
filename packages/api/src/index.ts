d28cb52e7059ab6ac5a2abdd27f1086a1e7a3992
TomasSanchezNegrette01 <tsancheznegrette@frba.utn.edu.ar>
2023-04-16 23:45:18 -0300

    Added background animation

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,7 +2,7 @@ import { createServer } from "http";
 import api from "./api";
 
 const server = createServer(api);
-const PORT = process.env.PORT || 3101;
+const PORT = process.env.PORT || 2225;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
